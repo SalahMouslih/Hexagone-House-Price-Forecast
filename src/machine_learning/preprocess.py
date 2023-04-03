1e7be8659695f85aa707b741801c47b0b2bf0fbb
salahddin <salah-eddine.elmouslih@ensae.fr>
2023-04-03 14:03:09 +0200

    add ml score function

--- src/machine_learning/preprocess.py
+++ src/machine_learning/preprocess.py
@@ -13,6 +13,60 @@ to_drop=['adresse_numero', 'adresse_suffixe','numero_disposition',
        'type_local','geometry','indices','quantile_prix','coeff_actu']
 
 
+def train_test_split(df, metropole=None, type_local=None, random_state=42, split=True, trimestres=['2021-T3', '2021-T4', '2022-T1', '2022-T2'], quartile=None):
+    """
+    Splits a given dataframe into training and testing sets based on the given parameters.
+
+    Args:
+        df (pandas.DataFrame): The dataframe to split.
+        metropole (str): The metropole to filter by.
+        type_local (str): The type of property to filter by.
+        random_state (int): The random state to use for shuffling the data.
+        split (bool): Whether to split the data into training and testing sets or not.
+        trimestres (list): The trimesters to exclude from the training set.
+        quartile (float): The quartile to use for filtering by price.
+
+    Returns:
+        tuple or pandas.DataFrame: If `split` is True, returns a tuple of (train_x, test_x, train_y, test_y). Otherwise, returns a pandas.DataFrame of the full split dataset.
+    """
+    # Check if quartile is a valid number between 0 and 1
+    if quartile is not None and not (0 <= quartile <= 1):
+        raise ValueError("Quartile must be a number between 0 and 1.")
+
+    # Filter by price quartile if quartile is specified
+    if quartile:
+        df = df[df.prix_m2_actualise < df.prix_m2_actualise.quantile(1 - quartile)][df.prix_m2_actualise > df.prix_m2_actualise.quantile(quartile)]
+
+    # Filter by metropole if specified
+    if metropole:
+        df = df[df['LIBEPCI'].str.contains(metropole)]
+
+    # Filter by type_local if specified
+    if type_local:
+        df = df[df['type_local'].str.contains(type_local)]
+
+    # Shuffle entire dataframe
+    shuffled_df = df.sample(frac=1, random_state=random_state)
+
+    # Split into training and testing sets if specified
+    if split:
+        # Select training data
+        train_df = shuffled_df[~shuffled_df['trimestre_vente'].isin(trimestres)]
+        train_x = train_df.drop('prix_m2_actualise', axis=1)
+        train_y = train_df['prix_m2_actualise']
+
+        # Select test data
+        test_df = shuffled_df.drop(train_df.index)
+        test_x = test_df.drop('prix_m2_actualise', axis=1)
+        test_y = test_df['prix_m2_actualise']
+
+        return train_x, test_x, train_y, test_y
+    else:
+        # Include test data in the full split dataset
+        test_df = shuffled_df.copy()
+        test_df['prix_m2_actualise'] = test_df['prix_m2']
+        return pd.concat([shuffled_df, test_df], axis=0)
+
 def pre_process(data, type_local):
     
     # Drop unnecessary columns
---
+++ src/machine_learning/scores.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+from machine_learning.preprocess import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.pipeline import Pipeline
+from joblib import dump
+
+QUARTILE = 0.05
+
+def train_score_save(model, data, metropole, type_local):
+    """
+    Trains a machine learning model, computes its performance score, and saves the trained model to disk.
+    
+    Args:
+        model (sklearn estimator): the machine learning model to be trained
+        data (pandas dataframe): the input data
+        metropole (str): the name of the metropolitan area
+        type_local (str): the type of local (e.g., apartment, house)
+        
+    Returns:
+        a pandas dataframe containing the performance metrics and characteristics of the input data
+    """
+    if not isinstance(data, pd.DataFrame):
+        raise ValueError('Input data should be a pandas dataframe.')
+    
+    if not isinstance(metropole, str) or not isinstance(type_local, str):
+        raise ValueError('Metropole and type_local should be strings.')
+    
+    try:
+        data = train_test_split(data, metropole, type_local, split=False)
+        data = preprocessing(data, type_local)
+        shape = data.shape
+        
+        clf = build_pipeline(model, data)
+        
+        X_train, X_test, y_train, y_test = train_test_split(data, split=True, quartile=QUARTILE)
+        clf.fit(X_train, y_train)
+        
+        best_score = clf.best_score_
+        best_params = clf.best_params_
+        
+        clf.fit(X_train, y_train)
+        y_predict = clf.predict(X_test)
+        
+        rmse = mean_squared_error(y_predict, y_test, squared=False)
+        score = r2_score(y_predict, y_test)
+        
+        error = pd.DataFrame((np.abs(y_test - y_predict) / y_test) * 100)
+        median = error.median()[0]
+        mean = error.mean()[0]
+        
+        shape = data.shape
+        estimator = clf.best_estimator_
+        dump(estimator, f"results/{metropole.strip().replace(' ', '-')}-{type_local}-{model}.joblib")
+        
+        result = f"{metropole}-{type_local}-{model}-best_score: {best_score}, best_param: {best_params}, rmse: {rmse}, score: {score}, median: {median}, mean: {mean}"
+        dataframe = pd.DataFrame([[metropole, type_local, model, best_score, best_params, rmse, score, median, mean, shape]],
+                                 columns=['metropole', 'type_local', 'model', 'best_score_cv_search', 'best_params', 'rmse', 'score_r2_test', 'error_prix_actualise_median', 'error_prix_actualise_mean', 'shape'])
+        with open("results_log/results.txt", "a+") as f:
+            f.write(result + '\n')
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        dataframe = pd.DataFrame()
+    
+    return dataframe
